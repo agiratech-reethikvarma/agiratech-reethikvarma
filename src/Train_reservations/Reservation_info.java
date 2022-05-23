12742e03cc4d964e522df0886a1b44790a04aae0
agiratech-reethikvarma <reethik.rv@agiratech.com>
2022-05-23 19:04:32 +0530

    updated on 23 may

--- src/Train_reservations/Reservation_info.java
+++ src/Train_reservations/Reservation_info.java
@@ -8,34 +8,37 @@ public class Reservation_info {
 	public int[] age;
 	public char[] gen;
 	public int PCount;
-	public String ASN,DSN;
+	public String ASN,DSN,RD;
 
 	Scanner s = new Scanner(System.in);
 
 	public void getInfo() {
 
-		System.out.print("\n\n Enter the number of Passengers : ");
+		System.out.print("\n\n Enter the number of Passengers :  ");
 		PCount = s.nextInt();
 		name = new String[PCount];
 		
 		age=new int[PCount];
 		gen=new char[PCount];
 		
-		getinfo();
+		Returninfo();
 		
-		System.out.print("Enter the Arrival Station name : ");
+		System.out.print ("\n Enter the Arrival Station name : ");
 		ASN=s.next();
-		System.out.print("Enter the Destination name : ");
+		System.out.print("\n Enter the Destination name : ");
 		DSN=s.next();
-		for (int i = 0; i < PCount; i++) {
-			System.out.println((i+1)+" Entered detatils are \n Name : " + name[i] + "\t Age:" + age[i] + "\t Gender : " + gen[i]+"\n Arraival Station : "+ASN+"\n Destination Station : "+DSN);
-		}
+		System.out.print("\n Enter the Date of journey in (YYYY-MM-DD) formate : ");
+		RD=s.next();
+		new Train_list(ASN,DSN,RD);
+		getReservationDetails();
+	
 	}
 
-	public void getinfo() {
+	public void Returninfo() {
 		
 		for (int i = 0; i < PCount; i++) {
-			System.out.print((i+1)+" Enter Your Name : ");
+			System.out.println(i+1);
+			System.out.print(" Enter Your Name : ");
 			name[i] = s.next();
 			System.out.print(" Enter Your Age : ");
 			age[i] = s.nextInt();
@@ -43,11 +46,20 @@ public class Reservation_info {
 			gen[i] = s.next().charAt(0);
 		}
 	}
+	
+	public void getReservationDetails(){
+		for (int i = 0; i < PCount; i++) {
+			System.out.println("\n");
+			System.out.println((i+1)+" Entered detatils are \n Name : " + name[i] + "\t Age:" + age[i] + "\t Gender : " + gen[i]+"\n Arraival Station : "+ASN+"\n Destination Station : "+DSN+"\n Reserved Date: "+RD);
+		}
+	}
 
 	public static void main(String[] args) {
 		Reservation_info r = new Reservation_info();
 		System.out.println("--- Welcome to Train Ticket Reservation Counter  ---");
 		r.getInfo();
+//		
+	
 
 	}
 
--- src/Train_reservations/Train_list.java
+++ src/Train_reservations/Train_list.java
@@ -1,66 +1,74 @@
 package Train_reservations;
 
 import java.util.Set;
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-//import java.util.ArrayList;
 import java.util.HashMap;
-//import java.lang.Iterable;
-
 public class Train_list {
 
 //  List of Trains are Created and Stored in Hashtable.
-	public void TrainSList() {
+	public Train_list(String as, String ds, String rd) {
 
-	HashMap<String, String[]> TrainList =new HashMap<String, String[]>();
+		HashMap<String, String[]> TrainList = new HashMap<String, String[]>();
 		String[] TR1, TR2, TR3, TR4, TR5, TR6, TR7, TR8, TR9, TR10;
-//		Date date;
-		
-		String pattern = "MM-dd-yyyy";
-		SimpleDateFormat sd = new SimpleDateFormat(pattern);
-	
-		String date = sd.format(new Date(2020,11,1));
-		System.out.println(date);
-//		String d=sd.toString();
-		System.out.println("date is :"+date);
-//		System.out.println("date is :"+d);
-		
-		TR1 = new String[] { "CHENNAI", "AVADI", "PERAMBUR", "THIRUVALLUR",date};
-		TR2 = new String[] { "THIRUVALLUR", "PERAMBUR", "AVADI", "CHENNAI",date };
-		TR3 = new String[] { "CHENGALPAT", "MARAIMALLAINAGAR", "KATTANGULATTHUR", "POTHERI", "THAMBARAM" };
-		TR4 = new String[] { "BEACH", "MAMBALAM", "CHENNAIPARK", "THAMBARAM" };
-		TR5 = new String[] { "GUINDY", "PALAVARAM", "CROMPET", "THAMBARAM",(new Date(2022-1900,12-1,24)).toString() };
-		TR6 = new String[] { "BEACH", "CHENNAIPORT", "NUNGAMBAKAM", "SAIDAPET", "CHENGALPAT" };
-		TR7 = new String[] { "CENTRAL", "AMBATHUR", "PERAMBUR", "VILLIVAKAM", "AVADI" };
-		TR8 = new String[] { "AVADI", "VELACHERI", "PATTRAVAKKAM", "ROIAPURAM" };
-		TR9 = new String[] { "CHEPAUK", "MADAVELI", "THARAMANI", "VELACHERI" };
-		TR10 = new String[] { "ARAKONAM", "THIRUVALLUR", "AVADI", "VILLIVAKKAM", "CHENNAI" };
+
+		TR1 = new String[] { "CHENNAI", "AVADI", "PERAMBUR", "THIRUVALLUR", "2022-12-10" };
+		TR2 = new String[] { "THIRUVALLUR", "PERAMBUR", "AVADI", "CHENNAI", "2022-12-11" };
+		TR3 = new String[] { "CHENGALPAT", "MARAIMALLAINAGAR", "KATTANGULATTHUR", "THAMBARAM", "2022-12-10" };
+		TR4 = new String[] { "BEACH", "MAMBALAM", "CHENNAIPARK", "THAMBARAM", "2022-12-10" };
+		TR5 = new String[] { "GUINDY", "PALAVARAM", "CROMPET", "THAMBARAM", "2022-12-10" };
+		TR6 = new String[] { "BEACH", "CHENNAIPORT", "NUNGAMBAKAM", "SAIDAPET", "CHENGALPAT", "2022-12-10" };
+		TR7 = new String[] { "CENTRAL", "AMBATHUR", "PERAMBUR", "VILLIVAKAM", "AVADI", "2022-12-11" };
+		TR8 = new String[] { "AVADI", "VELACHERI", "PATTRAVAKKAM", "ROYAPURAM", "2022-12-09" };
+		TR9 = new String[] { "CHEPAUK", "MADAVELI", "CHENNAI", "THARAMANI", "VELACHERI", "2022-12-09" };
+		TR10 = new String[] { "ARAKONAM", "THIRUVALLUR", "AVADI", "VILLIVAKKAM", "CHENNAI", "2022-12-10" };
+
 		TrainList.put("Train 1", TR1);
 		TrainList.put("Train 2", TR2);
 		TrainList.put("Train 3", TR3);
 		TrainList.put("Train 4", TR4);
 		TrainList.put("Train 5", TR5);
 		TrainList.put("Train 6", TR6);
-		
 		TrainList.put("Train 7", TR7);
 		TrainList.put("Train 8", TR8);
 		TrainList.put("Train 9", TR9);
 		TrainList.put("Train 10", TR10);
 
 		Set<String> Trains = TrainList.keySet();
-		for (String Key : Trains) {
-			System.out.println("## " + Key + " ##");
-			for (String train : TrainList.get(Key)) {
-				System.out.print(" -" + train + "- ");
 
+		int st1 = 0, st2 = 0;
+		int st3 = 0;
+		System.out.println("\n");
+		for (String Key : Trains) {
+			for (String train : TrainList.get(Key)) {
+				if (train.equals(as)) {
+					st1 = 1;
+
+				}
+				if (train.equals(ds)) {
+					st2 = 1;
+
+				}
+				if (train.equals(rd)) {
+					st3 = 1;
+
+				}
 			}
-			System.out.println();
+
+			if (st1 == 1 && st2 == 1 && st3 == 1) {
+				System.out.print(Key);
+
+				for (String train : TrainList.get(Key)) {
+					System.out.print(" -" + train + "-");
+				}
+				System.out.println();
+			}
+
+			st1 = 0;
+			st2 = 0;
+			st3 = 0;
 
 		}
 
+//		System.out.println("Available Trains are listed above Please select any from list.");
 	}
-	public static void main(String[] args) {
-		Train_list t=new Train_list();
-		t.TrainSList();
-	}
+
 }
